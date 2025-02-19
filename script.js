0846a6e5b686871c754b6bbe446f9f3a013415ef
akrawczyk1 <71944688+akrawczyk1@users.noreply.github.com>
2025-02-19 13:17:41 -0500

    Finishes initial version of game
    The game is played best 3 out of 5.
    There is error handling for invalid entry
    Any entry that begins with the correct letter will be accepted by the game
    There is a quit function that allows the player to exit the game.

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose \"rock\", \"paper\", or \"scissors\"! \n");
+// returns the first lower case letter of choice
+function sanitizeChoice(choice) {
     choice = choice.toLowerCase();
 
-    choice = choice.slice(0, 1);
+    return choice.slice(0, 1);
+}
+
+function unsanitizeChoice(choice){
+    switch (choice){
+        case "r": return "Rock";
+        case "p": return "Paper";
+        case "s": return "Scissors";
+        default: throw new Error("Attempted to unsanitize an invalid choice!");
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose \"rock\", \"paper\", or \"scissors\"! Or type \"q\" to quit! \n");
+    
+    choice = sanitizeChoice(choice);
 
     switch (choice){
         case "r":
@@ -22,17 +37,116 @@ function getHumanChoice() {
         case "s":
             console.log("You chose scissors!");
             break;
+        case "q":
+            break;
         default:
-            console.log("You made an invalid choice!");
-            choice = undefined;
+            throw new Error("Player made an invalid choice!");
 
     }
 
     return choice;
 }
 
-let choice = undefined;
+// Encodes "rock" to 0, "paper" to 1, and "scissors" to 2.
+function encodeChoice(choice){
+    if (choice === 0 || choice === 1 || choice === 2){
+        return choice;
+    }
 
-while (choice === undefined){
-    choice = getHumanChoice();
+    choice = sanitizeChoice(choice);
+    switch(choice){
+        case "r": return 0;
+        case "p": return 1;
+        case "s": return 2;
+        case "q": return -1;
+        default: throw new Error("An invalid choice was attempted to be encoded!");
+    }
 }
+
+function unencodeChoice(choice){
+    switch(choice){
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
+        default: throw new Error("Attempted to unencode invalid choice!");
+    }
+}
+
+function playRound(playerChoice, cpuChoice){
+    
+    try{
+        playerChoice = encodeChoice(playerChoice);
+        cpuChoice = encodeChoice(cpuChoice);
+
+        if (playerChoice === -1){
+            return -99;
+        }
+    
+
+        if (playerChoice == cpuChoice){
+            console.log("It's a draw! " + unencodeChoice(playerChoice) + " was chosen by both players!\n");
+            return 0;
+        }
+
+        else if ((playerChoice + 1) % 3 == cpuChoice){
+            console.log("You lose! Your opponent chose " + unencodeChoice(cpuChoice) + " and you chose " + unencodeChoice(playerChoice) + "!\n");
+            return -1;
+        }
+
+        else{
+            console.log("You win! Your opponent chose " + unencodeChoice(cpuChoice) + " and you chose " + unencodeChoice(playerChoice) + "!\n");
+            return 1;
+        }
+
+    } catch(e){
+        throw new Error(e.message);
+    }
+    
+
+}
+
+console.log("Let's play a game of rock, paper, scissors! Best 3 out of 5!");
+
+let playerScore = 0;
+let cpuScore = 0;
+let result;
+while (playerScore < 3 && cpuScore < 3){
+    
+
+    try{
+        let cpuChoice = getComputerChoice();
+        let playerChoice = getHumanChoice();
+        result = playRound(playerChoice, cpuChoice);
+    } 
+    catch(e){
+        console.error(e.message);
+        console.log("Let's try again!\n");
+        result = 0;
+        continue;
+    }
+    if (result === 0){
+        console.log("The game was a draw! Play again!\n");
+        continue;
+    }
+    else if (result === 1){
+        console.log("The overall score is now: \nPlayer -  " + ++playerScore + "\nComputer - " + cpuScore);
+    }
+    else if (result === -99){
+        console.log("Quitting the game!");
+        break;
+    }
+    else {
+        console.log("The overall score is now: \nPlayer -  " + playerScore + "\nComputer - " + ++cpuScore);
+    }
+}
+
+if (playerScore > cpuScore){
+    console.log("You win the match! The final score was " + playerScore + " to " + cpuScore);
+}
+else if (playerScore < cpuScore){
+    console.log("You lose the match... The final score was " + cpuScore + " to " + playerScore);
+}
+else{
+    console.log("The match was a draw! The final score is tied at " + playerScore);
+}
+
